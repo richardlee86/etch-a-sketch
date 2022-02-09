53b8824bc68434ee3a7e20f13d019fa5572cbec2
richardlee86 <richard.t.lee86@gmail.com>
2022-02-09 16:05:12 +0100

    added buttons for different modes and radio buttons to select size of box. Need functionality for radio buttons next

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const colorPicker = document.querySelector('#colorPicker');
 const container = document.querySelector('.container'); 
 const reset = document.querySelector('.reset');
 reset.addEventListener('click', function(){
-    location.reload(true); 
+    location.reload(true);     
 })
  
 //create a random RGB color
@@ -18,15 +18,15 @@ const rainbowRGB = () => {
 
 
 //create divs and append them to container div
-const addDivs = () => {
+const addDivs = (size) => {
     for (let i = 0; i < 1600; i++) {
         const div = document.createElement('div');
         div.className = 'etchDiv';
-        div.style.backgroundColor = 'red'; 
-        div.style.width = '16px';
-        div.style.height = '16px';  
+        div.style.backgroundColor = 'white'; 
+        div.style.width = size;
+        div.style.height = size;  
         container.appendChild(div);   
-        div.addEventListener('mouseover', function(){div.style.backgroundColor = 'blue';});   
+        div.addEventListener('mouseover', function(){div.style.backgroundColor = 'black';});   
     }     
     
 }
@@ -35,9 +35,9 @@ const addRainBowDivs = () => {
     for (let i = 0; i < 1600; i++) {
         const div = document.createElement('div');
         div.className = 'etchDiv';
-        div.style.backgroundColor = 'red'; 
-        div.style.width = '16px';
-        div.style.height = '16px';  
+        div.style.backgroundColor = 'white'; 
+        div.style.width = size;
+        div.style.height = size;  
         container.appendChild(div);   
         div.addEventListener('mouseover', function(){
             div.style.backgroundColor = rainbowRGB();
@@ -45,6 +45,23 @@ const addRainBowDivs = () => {
     }     
 }
 
-window.onload = () => {
-    addDivs(); 
-};
+
+
+let x = document.querySelector('#rainbow'); 
+let y = document.querySelector('.standard');
+let sixteen = document.querySelector('#sixteen');
+let sixtyFour = document.querySelector('#sixtyFour');
+let oneTwenty = document.querySelector('#oneTwenty');
+
+x.addEventListener('mousedown', function(){ 
+        location.reload(true);                 
+});
+
+x.addEventListener('mouseup', function(){addRainBowDivs()});
+
+y.addEventListener('mousedown', function(){    
+    location.reload(true);
+});
+
+y.addEventListener('mouseup', () => addDivs());
+
