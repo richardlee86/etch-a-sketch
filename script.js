427ae10a50ad3e50135b082029c8d7ccc4c0f9a5
richardlee86 <richard.t.lee86@gmail.com>
2022-02-10 16:39:51 +0100

    need to finish logic to change size of pixels on board

--- script.js
+++ script.js
@@ -19,25 +19,27 @@ const rainbowRGB = () => {
 
 //create divs and append them to container div
 const addDivs = (size) => {
-    for (let i = 0; i < 1600; i++) {
+    for (let i = 0; i < 1600; i++) {        
+        
         const div = document.createElement('div');
         div.className = 'etchDiv';
         div.style.backgroundColor = 'white'; 
-        div.style.width = size;
-        div.style.height = size;  
+        div.style.width = size + 'px';
+        div.style.height = size + 'px'; 
+        console.log(div.style.width); 
         container.appendChild(div);   
         div.addEventListener('mouseover', function(){div.style.backgroundColor = 'black';});   
     }     
     
 }
 
-const addRainBowDivs = () => {
+const addRainBowDivs = (size) => {
     for (let i = 0; i < 1600; i++) {
         const div = document.createElement('div');
         div.className = 'etchDiv';
         div.style.backgroundColor = 'white'; 
-        div.style.width = size;
-        div.style.height = size;  
+        div.style.width = size + 'px';
+        div.style.height = size + 'px';  
         container.appendChild(div);   
         div.addEventListener('mouseover', function(){
             div.style.backgroundColor = rainbowRGB();
@@ -53,15 +55,25 @@ let sixteen = document.querySelector('#sixteen');
 let sixtyFour = document.querySelector('#sixtyFour');
 let oneTwenty = document.querySelector('#oneTwenty');
 
+console.log(sixteen.checked);
+
 x.addEventListener('mousedown', function(){ 
         location.reload(true);                 
 });
 
-x.addEventListener('mouseup', function(){addRainBowDivs()});
+x.addEventListener('mouseup', function(){
+    if(sixteen.checked){
+        addRainBowDivs(21.25);
+    } else if (sixtyFour.checked){
+        addRainBowDivs(5.32);
+    } else if(oneTwenty.checked){
+        addRainBowDivs(2.83);
+    }
+});
 
 y.addEventListener('mousedown', function(){    
     location.reload(true);
 });
 
-y.addEventListener('mouseup', () => addDivs());
+y.addEventListener('mouseup', () => addDivs(21.25));
 
